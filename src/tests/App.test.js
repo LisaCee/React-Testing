d030e70147f565bcc0f7ecbd4d90fa339d2da40c
LisaCee <lisaceedesign@gmail.com>
2018-07-09 20:22:06 -0700

    started app tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,4 +11,14 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('renders correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should start at 0', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.state.total).toBe("0");
+  })
 });
